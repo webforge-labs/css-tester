93fabe9bca068a74c48e50d670b562d42b541a76
Philipp Scheit <p.scheit@ps-webforge.com>
2014-03-27 10:42:03 +0100

    port first methods from php to js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ module.exports = function(chai) {
 
     this.context = context;
     this.selector = selector;
+    this.debugSelector = undefined; // setter injection
 
+    assert.isFunction($, 'the first argument passed new CSSTest() has to be the jQuery function');
 
     // the element
 
@@ -17,15 +19,28 @@ module.exports = function(chai) {
         var parent = that.context instanceof CSSTest ? that.context.element() : that.context;
 
         $element = $(selector, parent);
+
+        // use chai inspect hook, todo magic things:
+        // https://github.com/chaijs/chai/blob/master/lib/chai/utils/inspect.js#L63
+        $element.inspect = function() {
+          return "$('"+that.getDebugSelector()+"')";
+        };
       }
 
       return $element;
     };
 
+    this.getDebugSelector = function() {
+      return that.debugSelector || that.selector;
+    };
+
     // traversion
 
-    this.css = function(selector, context) {
-      return new CSSTest($, selector, that);
+    this.css = function(childSelector, context) {
+      var cssTest = new CSSTest($, childSelector, that);
+      cssTest.debugSelector = selector+' '+childSelector;
+
+      return cssTest;
     };
 
     this.end = function() {
@@ -34,18 +49,30 @@ module.exports = function(chai) {
 
     // expectations
 
+    var expectElement = function(message) {
+      return expect(that.element(), message);
+    };
+
     this.count = function(expected, message) {
       assert.isNumber(expected);
-      expect(that.element(), message).to.have.length(expected);
+      expectElement(message).to.have.length(expected);
+      return this;
     };
 
     this.exists = function(message) {
-      expect(that.element(), message).to.have.length.of.at.least(1);
+      expectElement(message).to.have.length.of.at.least(1);
+      return this;
     };
 
     this.atLeast = function(expected, message) {
       assert.isNumber(expected);
-      expect(that.element(), message).to.have.length.of.at.least(expected);
+      expectElement(message).to.have.length.of.at.least(expected);
+      return this;
+    };
+
+    this.containsText = function(expected, message) {
+      expect(that.element().text(), message).to.contain(expected);
+      return this;
     };
   };
 
